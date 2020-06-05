d2c93674b091029f9a7d1b22f6833007522d2d97
Marcin <marroz73@wp.pl>
2020-06-05 21:47:10 +0200

    2-opt

--- algorithm.py
+++ algorithm.py
@@ -3,8 +3,6 @@ import csv
 import pandas as pd
 import numpy as np
 
-# using sieve of eratosthenes
-
 
 def sieve_of_eratosthenes(n):
     primes = [True for i in range(n+1)]  # Start assuming all numbers are primes
@@ -19,6 +17,15 @@ def sieve_of_eratosthenes(n):
     return primes
 
 
+def single_distance(dfcity, path, prev_city):
+    city_num = prev_city+1
+    step_num = city_num
+    result = np.sqrt(pow((dfcity.X[path[city_num]] - dfcity.X[path[prev_city]]), 2) +
+                     pow((dfcity.Y[path[city_num]] - dfcity.Y[path[prev_city]]), 2)) * \
+                    (1 + 0.1*((step_num % 10 == 0)*int(not(prime_cities[prev_city]))))
+    return result
+
+
 def total_distance(dfcity, path):
     prev_city = path[0]
     distance = 0
@@ -26,21 +33,21 @@ def total_distance(dfcity, path):
     for city_num in path[1:]:
         next_city = city_num
         distance = distance + \
-            np.sqrt(pow((dfcity.X[city_num] - dfcity.X[prev_city]), 2) + pow((dfcity.Y[city_num] - dfcity.Y[prev_city]), 2)) * \
-            (1 + 0.1*((step_num % 10 == 0)*int(not(prime_cities[prev_city]))))
+            np.sqrt(pow((dfcity.X[city_num] - dfcity.X[prev_city]), 2) +
+                    pow((dfcity.Y[city_num] - dfcity.Y[prev_city]), 2)) * \
+                   (1 + 0.1*((step_num % 10 == 0)*int(not(prime_cities[prev_city]))))
         prev_city = next_city
         step_num = step_num + 1
     return distance
 
 
-def nearest_neighbour():
-    cities = pd.read_csv("input/cities.csv")
-    ids = cities.CityId.values[1:]
-    xy = np.array([cities.X.values, cities.Y.values]).T[1:]
+def nearest_neighbour(dfcity):
+    ids = dfcity.CityId.values[1:]
+    xy = np.array([dfcity.X.values, dfcity.Y.values]).T[1:]
     path = [0, ]
     while len(ids) > 0:
-        #print(len(ids))
-        last_x, last_y = cities.X[path[-1]], cities.Y[path[-1]]
+        print(len(ids))
+        last_x, last_y = dfcity.X[path[-1]], dfcity.Y[path[-1]]
         dist = ((xy - np.array([last_x, last_y]))**2).sum(-1)
         nearest_index = dist.argmin()
         path.append(ids[nearest_index])
@@ -50,10 +57,47 @@ def nearest_neighbour():
     return path
 
 
-df_cities = pd.read_csv('input/cities.csv')
-prime_cities = sieve_of_eratosthenes(max(df_cities.CityId))
+def opt2(df_city, path, simplified):
+    swap = False
+    best_path = path
+    best_improvement = 0
+    for i in range(1, (len(path)-1)):
+        print(i)
+        for j in range(i+2, (len(path)-1)):
 
-nnpath = nearest_neighbour()
+            start = single_distance(df_city, path, i-1) + single_distance(df_city, path, j-1)
+            selected_path = []
+            selected_path += path[:i]
+            selected_path += list(reversed(path[i:j]))
+            selected_path += path[j:]
+            finish = single_distance(df_city, selected_path, i-1) + single_distance(df_city, selected_path, j-1)
+            improvement = start - finish
+            if best_improvement < improvement:
+                best_path = selected_path
+                best_improvement = improvement
+                swap = True
+            if simplified and swap:
+                break
+        if simplified and swap:
+            break
+    if swap:
+        print("Next iteration of 2-opt?(y/n)")
+        answer = input()
+        if answer == "y":
+            return opt2(df_city, best_path, simplified)
+    return best_path
+
+
+df_cities = pd.read_csv('input/cities2.csv')
+prime_cities = sieve_of_eratosthenes(max(df_cities.CityId))
+# dumbest_path = list(df_cities.CityId[:].append(pd.Series([0])))
+# print(total_distance(df_cities, dumbest_path))
+# path = opt2(df_cities, dumbest_path)
+# print(path)
+# print(total_distance(df_cities, path))
+
+
+nnpath = nearest_neighbour(df_cities)
 
 nnpath_with_primes = nnpath.copy()
 for index in range(20, len(nnpath_with_primes)-30):
@@ -68,3 +112,5 @@ for index in range(20, len(nnpath_with_primes)-30):
                 nnpath_with_primes = tmp_path.copy()
                 break
 print('Total distance with the Nearest Neighbor With Prime Swaps ' + "is {:,}".format(total_distance(df_cities, nnpath_with_primes)))
+path = opt2(df_cities, nnpath_with_primes, False)
+print(total_distance(df_cities, path))
--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,11 +1,12 @@
 import random
 from datetime import datetime
 
+
 class GeneticAlgorithm:
     def __init__(self, path):
         self.path = path
 
-    def PMX_method(self, subject_1, subject_2):
+    def pmx_method(self, subject_1, subject_2):
         random.seed(datetime.now())
         borders = random.sample(range(0, len(subject_1)), 2)
         borders.sort()
@@ -24,7 +25,7 @@ class GeneticAlgorithm:
         while True:
             for i in range(0, len(pairs)):
                 for k in range(i + 1, len(pairs)):
-                    if (pairs[i][1] == pairs[k][0]):
+                    if pairs[i][1] == pairs[k][0]:
                         pairs.append([pairs[i][0], pairs[k][1]])
                         del pairs[i]
                         del pairs[k - 1]
@@ -32,28 +33,28 @@ class GeneticAlgorithm:
             finished = True
             for i in range(0, len(pairs)):
                 for k in range(i + 1, len(pairs)):
-                    if (pairs[i][1] == pairs[k][0]):
+                    if pairs[i][1] == pairs[k][0]:
                         finished = False
                         break
-            if(finished):
+            if finished:
                 break
-        for i in range(0 ,len(pairs)):
-            if(pairs[i][0] == pairs[i][1]):
+        for i in range(0, len(pairs)):
+            if pairs[i][0] == pairs[i][1]:
                 del pairs[i]
         print('Przefiltrowane: ' + str(pairs))
 
         for i in range(0, len(subject_front_1)):
             for pair in pairs:
-                if(subject_front_1[i] == pair[1]):
+                if subject_front_1[i] == pair[1]:
                     subject_front_1[i] = pair[0]
-                if(subject_front_2[i] == pair[0]):
+                if subject_front_2[i] == pair[0]:
                     subject_front_2[i] = pair[1]
 
         for i in range(0, len(subject_end_1)):
             for pair in pairs:
-                if(subject_end_1[i] == pair[1]):
+                if subject_end_1[i] == pair[1]:
                     subject_end_1[i] = pair[0]
-                if(subject_end_2[i] == pair[0]):
+                if subject_end_2[i] == pair[0]:
                     subject_end_2[i] = pair[1]
         child_1 = subject_front_1 + subject_mid_2 + subject_end_1
         child_2 = subject_front_2 + subject_mid_1 + subject_end_2
@@ -61,8 +62,9 @@ class GeneticAlgorithm:
         print('Parent 2: ' + str(subject_2))
         print('Child 1: ' + str(child_1))
         print('Child 2: ' + str(child_2))
-        return (child_1, child_2)
+        return child_1, child_2
 
-obj = GeneticAlgorithm([1,2,3])
 
-obj.PMX_method([1,2,3,4,5,6,7], [5,4,6,7,2,1,3])
+obj = GeneticAlgorithm([1, 2, 3])
+
+obj.pmx_method([1, 2, 3, 4, 5, 6, 7], [5, 4, 6, 7, 2, 1, 3])
